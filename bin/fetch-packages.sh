cc9510b85e435a5d12025dcb1f4d733c23bf68f8
Matthew A. Brannigan <mbrannig@mbrannig.org>
2013-10-10 14:48:56 -0600

    * Resolve conflict

--- bin/fetch-packages.sh
+++ bin/fetch-packages.sh
@@ -2,11 +2,7 @@
 
 #VERSIONS="4.10.3 5.2.0 5.2.1"
 #VERSIONS="4.10.0 5.2.1"
-<<<<<<< HEAD
 VERSIONS="5.3.0 5.2.0 5.1.1"
-=======
-VERSIONS="5.3.0"
->>>>>>> origin/master
 ARCHES="i386 x86_64"
 ssh_host=pecan
 ssh_path="/nfs/netboot/sf-linux-os/Development"
